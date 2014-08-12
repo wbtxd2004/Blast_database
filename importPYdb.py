cf64d02b8ea23cf4cbb219f57b0b250fe0b362f0
WuBin <wbtxd2004@gmail.com>
2014-08-12 11:17:58 +0800

    Change file name in the code
    change some others name to PY..

--- importPYdb.py
+++ importPYdb.py
@@ -1,10 +1,10 @@
 #!/opt/local/bin/python
 #Filename: importPYdb.py
 from BioSQL import BioSeqDatabase
-server = BioSeqDatabase.open_database(driver = "MySQLdb",user = "chapmanb",passwd = "biopython", host = "localhost", db = "PYdatabase")
+server = BioSeqDatabase.open_database(driver = "MySQLdb",user = "chapmanb",passwd = "biopython", host = "localhost", db = "PY_db")
 db = server.new_database("PYDB")
 from Bio import GenBank
 parser = GenBank.FeatureParser()
-iterator = GenBank.Iterator(open("/Volumes/MACDATA/wubin/Workspace/Project/Blast_database/data/PY.structure.test.gb"),parser)
+iterator = GenBank.Iterator(open("/Volumes/MACDATA/wubin/Workspace/Project/Blast_database/data/PY.structure/PY800K.gb"),parser)
 db.load(iterator)
 server.adaptor.commit()
