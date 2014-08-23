898b9912b995aa3e41e878329bf8ccec3684cce0
WuBin <wbtxd2004@gmail.com>
2014-08-23 16:29:47 +0800

    Change data name and path.

--- importPYdb.py
+++ importPYdb.py
@@ -1,8 +1,8 @@
 #!/opt/local/bin/python
 #Filename: importPYdb.py
 from BioSQL import BioSeqDatabase
-server = BioSeqDatabase.open_database(driver = "MySQLdb",user = "chapmanb",passwd = "biopython", host = "localhost", db = "PY_db")
-db = server.new_database("PYDB")
+server = BioSeqDatabase.open_database(driver = "MySQLdb",user = "chapmanb",passwd = "biopython", host = "localhost", db = "pygenome")
+db = server.new_database("pygenome_db")
 from Bio import GenBank
 parser = GenBank.FeatureParser()
 iterator = GenBank.Iterator(open("/Volumes/MACDATA/wubin/Workspace/Project/Blast_database/data/PY.structure/PY800K.gb"),parser)
